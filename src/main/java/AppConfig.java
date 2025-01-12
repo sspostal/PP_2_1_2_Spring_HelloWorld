95456efb4659ee1218ba7b5c1f0b190721eaf043
Sergey <gonsoronss@icloud.com>
2025-01-12 10:57:20 +0300

    12.01

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -16,7 +16,7 @@ public class AppConfig {
     @Scope("prototype")
     public Cat getCat() {
         Cat cat = new Cat();
-        cat.setMessage("It's a cat!");
+        cat.setMessage("Catamaran");
         return cat;
     }
 
